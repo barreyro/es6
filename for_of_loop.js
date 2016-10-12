23446f84e8881b6a00944c802469b97454d0b2db
Stephanie Barreyro <stephaniebarreyro@gmail.com>
2016-10-12 13:36:49 -0400

    for-of loops

---
+++ for_of_loop.js
@@ -0,0 +1,40 @@
+/* 
+  * for... of loop
+  * it iterates over property values
+*/
+
+const countries = ['Canada', 'USA', 'Mexico'];
+
+// OLD WAY
+for(const index in countries) { // index
+  console.log(countries[index]);
+}
+
+// NEW WAY
+// 1. With arrays
+// for(const country of countries) { // value
+//    console.log(country);
+// }
+
+// 2. With objects
+// const countries = {
+//    canada: 'Canada',
+//    usa: 'USA',
+//    mexico: 'Mexico'
+// }; 
+
+// // Throws TypeError
+// for(const country of countries) { // value
+//    console.log('j', country);
+// }
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Symbol/iterator
+// countries[Symbol.iterator] = function () {
+//     let i = 0;
+//     return {
+//         next: function () {
+//             value: i++,
+//          done: i < 3
+//         }
+//     }
+// }
